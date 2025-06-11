193e603c20d5993c47cd4c1b7a7af52c6f4841e4
AR384 <wjdqor0060@naver.com>
2025-06-11 14:15:57 +0900

    노트북 에러 수정

--- mini_project/main.py
+++ mini_project/main.py
@@ -28,11 +28,11 @@ async def resize_image(background_tasks: BackgroundTasks, image: UploadFile = Fi
     img_path = ips.tmp_ImageSave(image,job_id)
     #비동기 백그라운드 테스크 시작
     background_tasks.add_task(inf.predict,img_path,job_id)
-    return JSONResponse(content={"message": "FastAPI 이미지 수신 완료", "filename": image.filename,'jobid':job_id})
+    return JSONResponse(content={"message": "[ FastAPI ]  이미지 수신 완료", "filename": image.filename,'jobid':job_id})
 
 @app.get("/result/{job_id}")
 async def get_result(job_id:str):
     if job_id not in results:
         logger.info("job_id not in results: 데이터를 찾을 수 없음")
-        return JSONResponse(status_code=404,content={"message":"FastAPI 작업아이디를 찾을 수 없습니다"})
+        return JSONResponse(status_code=404,content={"message":"[ FastAPI ] 작업아이디를 찾을 수 없습니다"})
     return results[job_id]
