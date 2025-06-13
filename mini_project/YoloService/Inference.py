d485f744942fea1483ef5a6fa039fa04dfefa524
AR384 <wjdqor0060@naver.com>
2025-06-13 15:45:54 +0900

    학교에서집으로커밋

--- mini_project/YoloService/Inference.py
+++ mini_project/YoloService/Inference.py
@@ -1,54 +1,97 @@
 from ultralytics import YOLO
+from ultralytics.engine.results import Results
 import cv2
 import numpy as np
 from YoloService import PostProcessing,PreProcessing
 from pathlib import Path
 import os
 import logging
-
+from shapely.geometry import Polygon
 
 class ImageInference:
     def __init__(self,results_store):
+        logging.basicConfig(level=logging.INFO)
+        self.logger = logging.getLogger('ImageInference')
         self.ips = PreProcessing.ImageProcessor()
         self.pps = PostProcessing.PostProcessing()
         self.save_path = Path.cwd() / 'img' / 'result'
-        
-        self.model_path = '../model/yolo11/Seg/yolo11n-seg.pt'
+        self.model_path = '../model/yolo11/Seg/yolo11x-seg.pt'
         self.model = YOLO(self.model_path)
         self.results_store = results_store
-        logging.basicConfig(level=logging.INFO)
+        self.inference_result =Results
+        self.b64_img = ""
+        self.save_file =""
+        self.pointlist = []
+        self.pred_name = []
         
-    def predict(self,img_path,job_id):
-        img = self.ips.resize(img_path,480)
-        
+    def sequence(self,img_path,job_id):
+        self.logger.info('sequence - 시퀀스 시작')
+        self.__predict(img_path,job_id)
+        self.__result_IMG_saving_coverting(img_path)
+        self.__result_sorting(job_id)
+        self.__result_push(job_id,)
+        self.logger.info(f'Jo_id ==={job_id}')
+        self.logger.info('sequence - 시퀀스 종료')
+    
+    def __predict(self,img_path,job_id):
         results = self.model.predict(
-            img,
+            img_path,
             conf=0.5,
             imgsz=(640,480),
             device='cuda:0',
             max_det=10,
             retina_masks=True,
-            )
-        
-        filename = str(img_path).replace('tmp','result')
-        save_file = self.save_path / filename
-        
-        print('save_file',save_file)
-        print('img_path',img_path)
-        #이미지 결과 저장
-        for i, result in enumerate(results):
+        )
+        self.inference_result=results
+        self.logger.info('__predict - 추론완료')
+
+    def __result_IMG_saving_coverting(self,img_path):
+        filename = str(img_path).replace('display','result')
+        self.save_file = str(self.save_path / filename) 
+        print('save_file : ',self.save_file)
+        print('img_path : ',img_path)
+        #결과 이미지 저장
+        for i, result in enumerate(self.inference_result):
             im_plot = result.plot()
-            cv2.imwrite(str(save_file), im_plot)
-        #이미지 json형태로 전환  
-        b64_img = self.pps.image_to_JSON(save_file)
+            cv2.imwrite(self.save_file, im_plot)
+        #display이미지 json형태로 전환  
+        self.b64_img = self.pps.image_to_JSON(img_path)
+        self.logger.info('__result_IMG_saving_coverting - 이미지 저장 및 json인코딩 완료')
+    
+    def __result_sorting(self,job_id):
+        #리스트에 작업이 없을 경우 작업
+        if job_id not in self.inference_result:
+            #전체 레이블 생성
+            labels = self.inference_result[0].names
+            for _,result in enumerate(self.inference_result):
+                #인식된 레이블 리스트 플롯
+                classified_names = result.boxes.cls.cpu().numpy()
+                #익식된 레이블의 폴리곤 만 추출
+                mask_coordinate = result.masks.xy
+                #레이블에서 값을 받아서 str 리스트로 저장
+                for i in classified_names:
+                    self.pred_name.append(labels[int(i)])
+                #레이블 별 폴리곤으로 리스트 생성
+                for poly in mask_coordinate:
+                    simplified_poly = self.ips.simplify_polygon(poly,tolerance=2.0)
+                    point_str = " ".join(f'{int(x)},{int(y)}' for x,y in simplified_poly)
+                    self.pointlist.append(point_str)
+                
+            self.logger.info('__result_sorting - 결과 정리 완료')
+    
+    def __result_push(self,job_id):
         #전역변수에 값 저장
         self.results_store[job_id] = {
             "message":'추론완료',
-            'file': f'{save_file}',
-            "image_base64": f"data:image/jpeg;base64,{b64_img}",
-            "status": "done"
+            'file': f'{self.save_file}',
+            "image_base64": f"data:image/jpeg;base64,{self.b64_img}",
+            "status": "done",
+            "poly": self.pointlist.copy(),
+            "names":self.pred_name.copy() #리스트 복사해서 새로운 객체로 만듬
         }
-        logger = logging.getLogger("Inferenc-predict()")
-        logger.info('추론완료 및 결과 저장됨')
-        # tmp 파일 삭제
-        
+        #리스트 초기화
+        self.pointlist=[]
+        self.pred_name=[]
+        self.logger.info('__result_push - 데이터 저장 완료')
+    
+    
--- mini_project/YoloService/PostProcessing.py
+++ mini_project/YoloService/PostProcessing.py
@@ -2,23 +2,27 @@ import cv2
 import base64
 import logging
 import os
-
+from YoloService import PreProcessing
 class PostProcessing:
-    def __init__(self):
+    def __init__(self,final_result=None):
         logging.basicConfig(level=logging.INFO)
-    
+        self.ips = PreProcessing.ImageProcessor()
+        self.logger = logging.getLogger("[ PostProcessing ]")
+        
     def image_to_JSON(self, img_path):
-        logger = logging.getLogger("[ PostProcessing-image_to_JSON ]")
-        logger.info("이미지 JSON 변환")
+        self.logger.info("image_to_JSON - 이미지 JSON 변환")
         with open(img_path,'rb') as f:
             img_bytes = f.read()
         b64_img = base64.b64encode(img_bytes).decode()
         return b64_img
     
     def temp_Image_Delete(self, img_path,foler_path):
+        self.logger.error("temp_Image_Delete - 파일을 삭제하였습니다.")
         delete_file = foler_path+'/'+img_path
         if os.path.isfile(delete_file):
             os.remove(delete_file)
         else:
-            logger = logging.getLogger("[ PostProcessing-temp_Image_Delete ]")
-            logger.error("파일을 삭제하지 못 하였습니다.")
+            self.logger.error("temp_Image_Delete - 파일을 삭제하지 못 하였습니다.")
+            
+    def user_selected_img(self,results,jobid,body):
+        self.ips.redraw_mask(results,jobid,body)
